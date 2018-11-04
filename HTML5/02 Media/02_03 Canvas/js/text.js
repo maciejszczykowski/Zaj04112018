bc9b0bd3976e2333d544a1a784b130e42cd533b9
Maciej Szczykowski <maciej.szczykowski@gmail.com>
2018-11-04 13:53:18 +0100

    Koniec Zajęć

---
+++ HTML5/02 Media/02_03 Canvas/js/text.js
@@ -0,0 +1,6 @@
+  myCanvas_context.textAlign = "center";
+  myCanvas_context.textBaseline = "middle";
+  myCanvas_context.fillStyle = "#7F7F7F";
+  myCanvas_context.font = "bold 30px sans-serif";
+  myCanvas_context.fillText("We're Growing", 155, 100);
+  // ten text pojawi sie na srodku wykresu, center....//
